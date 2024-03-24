10182ce294e196fabc0c7e521254087457c4e398
Janet Mensah <janetmensah60@gmail.com>
2024-03-24 15:57:16 +0000

    Created table for staff members along with foreign keys

--- Hospital_recruitment.sql
+++ Hospital_recruitment.sql
@@ -4,12 +4,11 @@
 -- This will be focusing specifically on the Staff Members, Department, Shift-Time, Shift Schedule and Hospital(if i have time)
 -- One hospital can have many departments
 
-
-
 create database hospital_recruitment;
 
 use hospital_recruitment;
 
+-- Table for Hospitals 
 create table Hospital(
 Hospital_id Integer Not Null Primary Key,
 Name Varchar(255) Not null,
@@ -20,6 +19,7 @@ phone_number Integer Not Null
 -- Renamed Hospital to Hospitals because there are many hospitals
 alter table Hospital rename Hospitals;
 
+-- Table for Departments
 create table Departments(
 Department_id Integer Not Null Primary Key,
 Name Varchar(155) Not null,
@@ -29,6 +29,19 @@ Constraint Hospital_id Foreign Key (Hospital_id) References Hospitals (Hospital_
 Unique (Phone_number)
 );
 
+-- Table for Staff members 
+create table Staff_Members(
+Staff_id Integer Not Null,
+FirstName varchar(50) NOT NULL,
+LastName varchar(50) NOT NULL,
+Phone_number Integer Not Null,
+Email_address VarChar(255),
+Age int not null check (age>=18),
+Role varchar(155),
+Clothes_size varchar(10),
+Department_id Integer Not Null,
+Hospital_id Integer Not Null,
+Foreign Key (Department_id) References Departments (Department_id),
+Foreign Key (Hospital_id) References Hospitals (Hospital_id)
+);
 
--- FirstName varchar(50) NOT NULL,
--- LastName varchar(50) NOT NULL
