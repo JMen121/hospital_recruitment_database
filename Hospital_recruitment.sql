cd631f3cb5e72027edbe186d01615a78c6bbb756
Janet Mensah <janetmensah60@gmail.com>
2024-03-24 15:02:21 +0000

    Created table for departments along with a foreign key

--- Hospital_recruitment.sql
+++ Hospital_recruitment.sql
@@ -20,16 +20,15 @@ phone_number Integer Not Null
 -- Renamed Hospital to Hospitals because there are many hospitals
 alter table Hospital rename Hospitals;
 
+create table Departments(
+Department_id Integer Not Null Primary Key,
+Name Varchar(155) Not null,
+Phone_number Integer,
+Hospital_id Integer Not Null,
+Constraint Hospital_id Foreign Key (Hospital_id) References Hospitals (Hospital_id),
+Unique (Phone_number)
+);
 
--- create table Departments(
--- Department_id Integer Not Null Primary Key,
--- name Varchar(155) Not null,
--- phone_number Integer,
--- Foreign Key (Hospital_id) References Hospital (Hospital_id)
-
-
-
--- );
 
 -- FirstName varchar(50) NOT NULL,
 -- LastName varchar(50) NOT NULL
