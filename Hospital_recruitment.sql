4ab5797cd283b87fdc45cc55e4b926d94ec7db38
Janet Mensah <janetmensah60@gmail.com>
2024-03-24 13:55:00 +0000

    Written the scenario and created the database for hospital recrutiment

--- Hospital_recruitment.sql
+++ Hospital_recruitment.sql
@@ -1,3 +1,19 @@
+
+-- The Scenario
+-- This is a hospital recruitment database of a local trust, consisting of 3 hospitals(if I have time). 
+-- This will be focusing specifically on the Staff Members, Department, Shift-Time, Shift Schedule and Hospital(if i have time)
+-- One hospital can have many departments
+
+
+
 create database hospital_recruitment;
 
 use hospital_recruitment;
+
+create table Hospital(
+Hospital_id Integer Not Null,
+Hospital_Name varchar(255) Not null,
+Location varChar(255) Not null,
+);
+-- FirstName varchar(50) NOT NULL,
+-- LastName varchar(50) NOT NULL
