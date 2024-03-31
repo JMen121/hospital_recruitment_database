9ef66b38b374f3f1da3483603ebce673eb2b19ae
Janet Mensah <janetmensah60@gmail.com>
2024-03-31 14:56:24 +0100

    Created query to see departments that phone numbers are null

--- Queries written.sql
+++ Queries written.sql
@@ -1,10 +1,5 @@
 -- My Queries
 
--- Two Aggregate Functions(sum, avg,)
--- Two inbuilt functions (count, floor, isnull,)
-
--- use majority of the queries data sorting
-
 --  Query to find out how what day each staff is working on, in what hospital
 SELECT sm.FirstName, sm.LastName, st.name AS Shift_Type, ss.Shift_Date, h.name AS Hospital, d.name AS Department
 FROM Staff_Member sm
@@ -17,18 +12,22 @@ ORDER BY shift_date;
 -- Using an aggregate function to find out how many hours David Choi has worked this week
 SELECT sm.FirstName, sm.LastName, SUM(ss.hours_worked) AS Total_Hours
 FROM Staff_member sm
-JOIN Staff_schedule ss ON sm.Staff_id = ss.Staff_id
+Inner JOIN Staff_schedule ss ON sm.Staff_id = ss.Staff_id
 WHERE sm.FirstName = 'David' AND LastName = 'Choi';
 -- answer is 27 hours
 
 -- Selecting Staff members in Good Health Hospital and identifying the average age in the hospital and rounding it to the nearest integer
 SELECT Floor(AVG(sm.age)) AS Average_Age, h.Name AS Hospital
 FROM Staff_Member sm
-JOIN Department d ON sm.Department_id = d.Department_id
-JOIN Hospital h ON d.Hospital_id = h.Hospital_id
+Inner JOIN Department d ON sm.Department_id = d.Department_id
+Inner JOIN Hospital h ON d.Hospital_id = h.Hospital_id
 WHERE h.Name = 'Good Health Hospital';
 
-
+-- The local Trust has realised that some departments have not written their phone numbers, but not sure which?
+Select d.name As department_name, d.phone_number, h.name as Hospital_name
+ From Department d
+ Left Join Hospital h ON d.hospital_id = h.hospital_id
+ where d.phone_number is null;
 
 -- Deleting shifts that are night or late in hospital 1
 DELETE FROM shift_type WHERE (name = 'Night shift' OR name = 'late shift') AND Hospital_id = 1;
